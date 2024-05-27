51f33d856dec43f962b2f3d1f2532c53ab6b8161
Owen Johnson <opjoh@gmail.com>
2024-05-27 15:33:21 -0500

    added component files

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,24 @@
+function Nav() {
+    return <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="#">About Me</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contact</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Portfolio</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Resume</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+}
+
+export default Nav;
