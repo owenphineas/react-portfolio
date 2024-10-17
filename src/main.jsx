362abb8efc645e24da5f5420ec3421415b711e84
Owen Johnson <opjoh@gmail.com>
2024-10-17 13:01:31 -0500

    added portfolio text

--- src/main.jsx
+++ src/main.jsx
@@ -17,6 +17,7 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
+        path: 'home',
         element: <Home />,
       },
       {
--- src/pages/ResumePage.jsx
+++ src/pages/ResumePage.jsx
@@ -1,29 +1,36 @@
 function Resume () {
     return <div className="resume">
-        <h4>Education</h4>
-        <div>
-            <p className="bold">Hamline University, Saint Paul, Minnesota</p>
-            <p>BA, English, May 2022</p>
+        <div className="resume-headers">
+            <h4 className="education-label">Education</h4>
+            <h4 className="languages-label">Languages and frameworks</h4>
+            <h4 className="technologies-label">Technologies</h4>
         </div>
-        <div>
-            <p className="bold">University of Minnesota, Minneapolis, Minnesota</p>
-            <p>Certificate, Full Stack Web Development, July 2024</p>
+        <div className="column">
+            <div className="colleges">
+                <div>
+                    <p className="bold">Hamline University, Saint Paul, Minnesota</p>
+                    <p>BA, English, May 2022</p>
+                </div>
+                <div>
+                    <p className="bold">University of Minnesota, Minneapolis, Minnesota</p>
+                    <p>Certificate, Full Stack Web Development, July 2024</p>
+                </div>
+            </div>
+            <div>
+                <ul className="resume-list languages">
+                    <li>JavaScript</li>
+                    <li>CSS</li>
+                    <li>React</li>
+                </ul>
+                <ul className="resume-list technologies">
+                    <li>SQL</li>
+                    <li>Node</li>
+                    <li>MongoDB/Mongoose</li>
+                    <li>Node</li>
+                </ul>
+            </div>
         </div>
-        <div>
-            <h4>Languages and frameworks</h4>
-            <ul className="languages">
-                <li>JavaScript</li>
-                <li>CSS</li>
-                <li>React</li>
-            </ul>
-            <h4>Technologies</h4>
-            <ul className="technologies">
-                <li>SQL</li>
-                <li>Node</li>
-                <li>MongoDB/Mongoose</li>
-                <li>Node</li>
-            </ul>
-        </div>
+        <div className="buffer-right"></div>
     </div>
 }
 
